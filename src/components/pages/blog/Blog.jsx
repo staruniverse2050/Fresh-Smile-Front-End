341e6720a4361e231355c6df4e86574722dba6b7
minjibyung <minjibyung@gmail.com>
2023-05-03 15:08:18 -0500

    se agrega apartado del blog

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import './Blog.css'
 
-
-export const handleClick = () => 
-  console.log('El botón ha sido presionado');
-
 export const Blog = () => {
   return (
 <>
 <div className="header-banner">
-  <img className="banner-img" src="public/banner.jpg" alt="" />
-<h1 class="title2">En este blog te brindaremos informacion sobre nuestras clinicas</h1>
-<button className="btn" onClick={handleClick}>Agenda tu cita</button>
+  <div className="container-banner">
+  <h1 className="container-title">En este blog te brindaremos informacion sobre nuestras clinicas</h1>
+  <div className="header-btn">
+  <button className="date-btn">Agenda tu cita</button>
 </div>
-
+</div>
+</div>
 
 <div>
 <div className="post-container">
@@ -49,7 +47,50 @@ export const Blog = () => {
         </div>
       </div>
       </div>     
-        
+
+
+<footer>
+<div class="footer-area">
+    <div class="container">
+
+    <div class="address">
+  <div class="container">
+    <div class="footer-content">
+      <div class="address-info">
+        <i class="fa-sharp fa-solid fa-location-dot"></i>
+      <p><span>Direccion</span></p><p className="dateT"><span>cra21a, calle 9na #099</span></p>
+</div>
+</div>
+
+  <div class="email">
+    <div class="container">
+      <div class="footer-content">
+      <div class="email-info">
+        <i class="fa-solid fa-envelope"></i>
+        <p><span>E-mail</span></p><p className="dateT"><span>freshsmile@gmail.com</span></p>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div class="phone">
+    <div class="container">
+      <div class="footer-content">
+      <div class="phone-info">
+        <i class="fa-solid fa-phone-volume"></i>
+        <p><span>Contactanos</span></p><p className="dateT"><span>3204415807</span></p>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  </div>
+</div>
+</div>
+</div>
+
+</footer>
+
     </>
   );
 };

0012ed4fff1be1c9f71380444997331798a0f872
juandajtl <personalisado2018@gmail.com>
2023-05-06 14:27:47 -0500

    arreglo del register y el login

--- src/components/layouts/Login/Login.jsx
+++ src/components/layouts/Login/Login.jsx
@@ -2,61 +2,64 @@ import React, { useState } from 'react';
 import './Login.css';
 
 export const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post('/login', { email, password });
-            console.log(response.data);
-        } catch (error) {
-            setError(error.response.data.message);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/login', { email, password });
+      console.log(response.data);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (email === '' || password === '') {
-            setError('Por favor, complete todos los campos.');
-        } else {
-            handleLogin();
-        }
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email === '' || password === '') {
+      setError('Por favor, complete todos los campos.');
+    } else {
+      handleLogin();
+    }
+  };
 
-    return (
-        <div className="LoginHeader">
-            <img className="iconoInicio" src="./public/agregar-usuario.png" alt="imagen de persona" />
-            <div className="Login">
-                <section className="LoginForm">
-                    <input
-                        className="LoginInput"
-                        type="text"
-                        placeholder="Ingrese su correo"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        className="LoginInput"
-                        type="password"
-                        placeholder="Ingrese su contraseña"
-                        required
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button className="LoginButton" onClick={handleSubmit}>
-                        Iniciar Sesion
-                    </button>
-                </section>
-                <p className="LoginP">
-                    ¿Aun no eres miembro?{' '}
-                    <a href="../Register/Register" className="LoginA">
-                        Registrate
-                    </a>
-                </p>
-                {error && <p>{error}</p>}
-            </div>
-        </div>
-    );
+  return (
+    <>
+      {/* Header sin barra */}
+      <div className="LoginHeader">
+        <img className="iconoInicio" src="./public/agregar-usuario.png" alt="imagen de persona" />
+      </div>
+      <div className="Login">
+        <section className="LoginForm">
+          <input
+            className="LoginInput"
+            type="text"
+            placeholder="Ingrese su correo"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="LoginInput"
+            type="password"
+            placeholder="Ingrese su contraseña"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="LoginButton" onClick={handleSubmit}>
+            Iniciar Sesion
+          </button>
+        </section>
+        <p className="LoginP">
+          ¿Aun no eres miembro?{' '}
+          <a href="../Register/Register" className="LoginA">
+            Registrate
+          </a>
+        </p>
+        {error && <p>{error}</p>}
+      </div>
+    </>
+  );
 };
--- src/components/layouts/Register/Register.jsx
+++ src/components/layouts/Register/Register.jsx
@@ -2,104 +2,84 @@ import React, { useState } from 'react';
 import './Register.css';
 
 export const Register = () => {
-    const [formData, setFormData] = useState({
-        identificationNumber: '',
-        firstName: '',
-        lastName: '',
-        address: '',
-        phone: '',
-        email: '',
-        password: '',
-    });
-    const [error, setError] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('/register', { name, email, password, confirmPassword });
+      console.log(response.data);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('/register', formData);
-            console.log(response.data);
-        } catch (error) {
-            setError(error.response.data.message);
-        }
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name === '' || email === '' || password === '' || confirmPassword === '') {
+      setError('Por favor, complete todos los campos.');
+    } else if (password !== confirmPassword) {
+      setError('Las contraseñas no coinciden.');
+    } else {
+      handleRegister();
+    }
+  };
 
-    return (
-        <div className="RegisterHeader ">
-            <img className="iconoInicio" src="./public/agregar-usuario.png" alt="imagen de persona" />
-            <div className="Register">
-                <section className="RegisterForm">
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="identificationNumber"
-                        placeholder="Ingrese su numero de identificación"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="firstName"
-                        placeholder="Ingrese su nombre"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="lastName"
-                        placeholder="Ingrese su apellido"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="address"
-                        placeholder="Ingrese su dirección"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="phone"
-                        placeholder="Ingrese su teléfono"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="text"
-                        name="email"
-                        placeholder="Ingrese su correo electrónico"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        className="RegisterInput"
-                        type="password"
-                        name="password"
-                        placeholder="Cree una contraseña"
-                        onChange={handleChange}
-                        required
-                    />
-                    <button className="RegisterButton" onClick={handleSubmit}>
-                        Registrate
-                    </button>
-                </section>
-                <p className="RegisterP">
-                    ¿Ya eres miembro?{' '}
-                    <a href="/login" className="RegisterA">
-                        Inicia tu sesión
-                    </a>
-                </p>
-                {error && <p>{error}</p>}
-            </div>
-        </div>
-    );
+  return (
+    <>
+      {/* Header sin barra */}
+      <div className="RegisterHeader">
+        <img className="iconoRegistro" src="/agregar-usuario.png" alt="imagen de persona" />
+      </div>
+      <div className="Register">
+        <section className="RegisterForm">
+          <input
+            className="RegisterInput"
+            type="text"
+            placeholder="Ingrese su nombre"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            className="RegisterInput"
+            type="text"
+            placeholder="Ingrese su correo"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="RegisterInput"
+            type="password"
+            placeholder="Ingrese su contraseña"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            className="RegisterInput"
+            type="password"
+            placeholder="Confirme su contraseña"
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button className="RegisterButton" onClick={handleSubmit}>
+            Registrarse
+          </button>
+        </section>
+        <p className="RegisterP">
+          ¿Ya eres miembro?{' '}
+          <a href="../Login" className="RegisterA">
+            Inicia Sesión
+          </a>
+        </p>
+        {error && <p>{error}</p>}
+      </div>
+    </>
+  );
 };

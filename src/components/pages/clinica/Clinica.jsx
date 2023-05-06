93ebb9274d913678f02eae0d6697df3ab5c144b1
minjibyung <minjibyung@gmail.com>
2023-05-06 12:26:37 -0500

    se agrega apartado clinica

--- src/components/pages/clinica/Clinica.jsx
+++ src/components/pages/clinica/Clinica.jsx
@@ -5,16 +5,47 @@ import './Clinica.css'
 export const Clinica = () => {
   return (
     <>
-     <div className="container_banner">
-        {/* <img className="" src="./public/banner.jpg" alt="" /> */}
-        <div className="container_home">
-          <h1>¡Bienvenido a la clinica Fresh Smile Cmills!</h1>
-          <div className="container-btn">
-            <button className="boton">Agendar Cita</button>
-            <button className="boton">Buscar Clinica</button>
-          </div>
-        </div>
-      </div>
+ 
+      
+        <h2 className="Sedes">nuestras sedes</h2>
+
+
+      <div className="container-sedes">
+        <img src="./public/armenia.jpg" alt="sede1" />
+        <h1>armenia</h1>
+        <h1>7:00am-12:30pm</h1>
+        <h1>sede central</h1>
+          <button className="botton-sede">Agendar tu Cita</button>
+         </div>
+      
+      <div className="container-sedes">
+        <img src="./public/tebaida.png" alt="sede2" />
+        <h1>Tebaida</h1>
+        <h1>7:00am-12:30pm</h1>
+        <h1>sede central</h1>
+          <button className="botton-sede">Agendar tu Cita</button>
+         </div>
+      
+      <div className="container-sedes">
+        <img src="./public/armenia.jpg" alt="sede3" />
+        <h1>circasia</h1>
+        <h1>7:00am-12:30pm</h1>
+        <h1>sede central</h1>
+         <button className="botton-sede">Agendar tu Cita</button>
+          </div> 
+
+ <div className="container-mapa">
+  <div className="mapa">
+  <img src="public/ubicación.jpg" alt="ubicacion" />
+  </div>
+
+  <div class="texto">
+    <h2>¡Aquí encuentras la ubicación de nuestra sede principal en armenia!</h2>
+  </div>
+
+  </div>
+  
     </>
+
   );
 };

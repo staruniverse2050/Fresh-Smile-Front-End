9914979b5ba4f35032ccaad288e76a30c3006cbd
fernanda11232 <fernandahenao868@gmail.com>
2023-05-11 15:39:45 -0500

    Agregando cambios de procedimientos

--- src/components/pages/procedimientos/Informacion1/Informacion1.jsx
+++ src/components/pages/procedimientos/Informacion1/Informacion1.jsx
@@ -5,66 +5,61 @@ import './informacion1.css'
 export const Informacion1 = () => {
   return (
     <>
-    
-      
-    
-    <div className="caja">
+    <div className="ca">
 
-<h2 className="letra">¿Por que es importante usar brackets?</h2>
-<div className="imgg">
-<img src="./public/prueba1.png" />
+<h2 className="ti">¿Qué es el blanqueamiento dental?</h2>
+<div className="fo">
+<img src="./public/blanqueamientoss.png" />
 </div>
 
 
-<p className="parra">La ortodoncia no es una cuestión meramente estética dirigida a obtener una dentadura perfecta. Muy al contrario, es una de las claves fundamentales de una buena salud bucodental.La ortodoncia es una especialidad que se ocupa de diagnosticar, corregir e incluso prevenir las anomalías (maloclusiones) en la forma, posición, relación y función de los dientes y los maxilares, la cara y los trastornos funcionales de la masticación. Entre tales anomalías figuran:
-La mala alineación o apiñamiento de los dientes.
-La posición adelanta o retrasada de las piezas dentales en relación a los maxilares.
-Desplazamiento de uno de los dos maxilares, etc.</p>
+<p className="pa">El blanqueamiento dental es un tratamiento que se aplica a los dientes que han cambiado de color, siendo uno de los tratamiento estéticos más conservadores con el tejido dentario. Con este tratamiento se consigue cambiar el tono del color de los dientes, dejando estos más blancos, luminosos y devolviendo el brillo natural del esmalte dental. Es decir, consiste en eliminar las manchas intrínsecas y extrínsecas que producen el cambio de tonalidad en la superficie dentaria.</p>
 </div>
 
 {/* CONTAINER1 */}
 
-<div className="caja1">
+<div className="ca1">
 
 
-<h2 className="letra1">Dientes torcidos</h2>
-<div className="img1">
-<img src="./public/torcido.png" />
+<h2 className="ti1">¿Cuándo puedo volver a tomar café después de un blanqueamiento?</h2>
+<div className="fo1">
+<img src="./public/cafee.png" />
 </div>
 
 
-<p className="parra1">Los dientes desalineados, los dientes torcidos o los dientes apiñados se ven comúnmente en personas de todas las edades. Cuando los dientes erupcionan y crecen de manera que se superponen, se retuercen, se inclinan o giran, se apiñan, se tuercen o se desalinean. Por supuesto, esto dificulta la limpieza de los dientes y los daña.</p>
+<p className="pa1">Evita el consumo de bebidas gaseosas durante los posteriores a 15 días al tratamiento. No consumas bebidas con colorantes, como el café, el té o el vino tinto, ya que pueden perjudicar gravemente al blanqueamiento. Para evitar que estas bebidas entren en contacto con los dientes, puedes utilizar una pajita. Evita frutas tales como la fresa, la cereza o la frambuesa para que no se manchen tus piezas dentales. Intenta no consumir cítricos, ya que su acidez puede afectar a tu esmalte.</p>
 </div>
 
 
 {/* CONTAINER2 */}
 
 
-<div className="caja2">
+<div className="ca2">
 
 
-<h2 className="letra2">Exceso de espacio entre los dientes</h2>
-<div className="img2">
-<img src="./public/separados.png" />
+<h2 className="ti2">¿Que no hacer cuando te hacen blanqueamiento?</h2>
+<div className="fo2">
+<img src="./public/dientesamarillos.png" />
 </div>
 
 
-<p className="parra2">El exceso de espacio entre los dientes, conocido como diastema, es en realidad una de las razones más comunes para usar frenillos. El espaciad es un exceso de espacio para los dientes que da como resultado espacios muy pronunciados entre los dientes. Esto generalmente ocurre cuando los dientes son más pequeños que el espacio disponible. El espaciamiento también puede ser causado por dientes protrusivos, dientes faltantes, dientes impactados o adherencias anormales de tejido a las encías.</p>
+<p className="pa2">No consumas cualquier bebida que deje manchas como el Té, Café, Vino rojo, bebidas alcohólicas y zumos de fruta.
+Evita todos los alimentos oscuros como Salsas de tomate, de soja, carne roja, chocolate y cualquier fruta excepto el plátano. No fumes. Podría perfectamente indicarte cuando puedes empezar a fumar o en qué periodos lo puedes hacer pero es de lógica que lo único que voy a decir aquí es NO FUMES. Saca tus propias conclusiones. Evita las alubias de color y las lentejas. Evita la leche de coco (pero con el agua de coco no pasa nada)</p>
 </div>
 
 
 {/* CONTAINER3 */}
 
-<div className="caja3">
+<div className="ca3">
 
 
-<h2 className="letra3">obremordida</h2>
-<div className="img3">
-<img src="./public/sobremordida.png"/> 
+<h2 className="ti3">¿Por qué se me ponen los dientes amarillos?</h2>
+<div className="fo3">
+<img src="./public/chata.png"/> 
 </div>
 
 
-<p className="parra3">Una sobremordida es cuando la mandíbula superior se superpone a la mandíbula inferior. Es común que algunos pacientes tengan una sobremordida leve de solo uno o dos milímetros, pero algunos tienen problemas de mordida más graves. Una maloclusión es una sobremordida profunda donde la superposición es más pronunciada. Puede provocar dolores de cabeza, ya que tienden a apretar la mandíbula y rechinar mucho los dientes.</p>
+<p className="pa3">Si no llevamos una correcta limpieza de los dientes, las bacterias acumuladas en ellos se cristalizarán y formarán una capa amarillenta llamada sarro. Esta capa pegajosa fomenta la aparición de más sarro y, además, altera la estética debido a su tono amarillento y marrón si se prolonga mucho en el tiempo. El no tratar a tiempo este problema de acumulación de sarro acarrea graves consecuencias a nivel de las encías, y en ultima instancia el hueso que rodea los dientes, lo que termina por producir la movilidad y hasta la caída de los dientes</p>
 </div>
 
 
@@ -72,20 +67,17 @@ Desplazamiento de uno de los dos maxilares, etc.</p>
 {/* CONTAINER4 */}
 
 
-<div className="caja4">
+<div className="ca4">
 
 
-<h2 className="letra4">Submordida</h2>
-<div className="img4">
-<img src="./public/submordida.png" /> 
+<h2 className="ti4">¿Es bueno blanquearse los dientes con bicarbonato y limón?</h2>
+<div className="fo4">
+<img src="./public/limon.png" /> 
 </div>
 
 
-<p className="parra4">Si sus dientes inferiores están demasiado hacia adelante o los dientes superiores están demasiado hacia atrás, es probable que sufra una mordida inferior. Una submordida es cuando cierra la boca y sus dientes frontales inferiores están frente a sus dientes superiores. En muchos casos, esto es en realidad un problema con la mandíbula inferior demasiado adelantada. Una mordida puede causar una apariencia facial desequilibrada y acelerar el envejecimiento. Los frenillos mueven los dientes superiores o inferiores, o tanto los dientes superiores como los inferiores, para corregir la mordida inferior. En casos severos, puede ser necesaria una cirugía de mandíbula junto con aparatos de ortodoncia para corregir la mordida inferior.</p>
-</div>
-
-
-    
+<p className="pa4">En el primer punto de este artículo, donde explicamos qué es el bicarbonato, hacemos mención a su efecto efervescente al ser juntado con un ácido. Esto nos puede dar pistas para responder a la pregunta que planteamos en este apartado. Si no recomendamos utilizar bicarbonato, desaconsejamos por completo utilizarlo junto a limón, ya que aumenta considerablemente su poder abrasivo y daña más rápidamente el esmalte dental y las mucosas orales.</p>
+</div>  
     </>
   )
 }

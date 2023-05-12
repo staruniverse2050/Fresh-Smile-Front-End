56c56fd306ceb19284a76a45fac5c86283a6f571
Maite Martinez <salomeydavid507@gmail.com>
2023-05-11 23:41:34 -0500

    Modificando enlaces de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,30 @@ import { Nosotros } from "./components/pages/nosotros/Nosotros";
 import { Register } from "./components/layouts/Register/Register";
 import { Blog } from "./components/pages/blog/Blog";
 import Contacto from "./components/pages/contacto/Contacto";
-import {Clinica} from "./components/pages/clinica/Clinica";
-import {Informacion1} from "./components/pages/procedimientos/Informacion1/Informacion1"
-import {Informacion2 } from "./components/pages/procedimientos/Informacion2/Informacion2";
+import { Clinica } from "./components/pages/clinica/Clinica";
+import { Informacion1 } from "./components/pages/procedimientos/Informacion1/Informacion1";
+import { Informacion2 } from "./components/pages/procedimientos/Informacion2/Informacion2";
 import { Informacion3 } from "./components/pages/procedimientos/Informacion3/Informacion3";
 import { Informacion4 } from "./components/pages/procedimientos/Informacion4/Informacion4";
-// import { Navigate } from "react-router-dom";
-
-
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-      {/* <Route path="/" element={<Navigate to="/Home" />} /> */}
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Procedimientos" element={<Procedimientos />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/Procedimientos" element={<Procedimientos />} />
         <Route path="/Register/Register" element={<Register />} />
         <Route path="/Nosotros" element={<Nosotros />} />
         <Route path="/Clinica" element={<Clinica />} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/contacto" element={<Contacto />} />
-        <Route path="/Informacion1" element={<Informacion1 />} /> 
+        <Route path="/Informacion1" element={<Informacion1 />} />
         <Route path="/Informacion2" element={<Informacion2 />} />
-        <Route path="/Informacion3" element={<Informacion3/>} />
-        <Route path="/Informacion4" element={<Informacion4/>} />
+        <Route path="/Informacion3" element={<Informacion3 />} />
+        <Route path="/Informacion4" element={<Informacion4 />} />
       </Routes>
+      <Footer />
     </>
   );
 }

c3eb635408af7a2935d1ef40cf8a59bb80a884b2
juandajtl <personalisado2018@gmail.com>
2023-06-13 17:30:13 -0500

    arreglo de icono-inicio

--- src/components/pages/PerfilAdministrador/PerfilAdministrador.jsx
+++ src/components/pages/PerfilAdministrador/PerfilAdministrador.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import moment from "moment";
+// import moment from "moment";
 import swal from "sweetalert";
 import "./perfiladministrador.css";
 
